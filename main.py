25c01d2d398c57a7dcf76b221a3a1feba9d12923
Nomikose <Dartwer55@gmail.com>
2023-06-04 22:27:00 +0300

    Phasa_4

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import requests
 
+# Get запрос - нет тела, но могут быть переданы параметры в строке запроса через ?
 # payload = {"name": "Sam"}
 # response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
 # print(response.text)
 
-response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
-# print(response.status_code)
+# Смотрим редирект по 301 коду ответа, а также вытаскиваем инфу из массива history
+# response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+# # print(response.status_code)
+# # print(response.text)
+#
+# first_response = response.history[0]
+# second_response = response
+# print(first_response.url)
+# print(second_response.url)
+
+# Смотрим заголовки
+# headers = {"some_header": "123"}
+# response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers = headers)
 # print(response.text)
+# print(response.headers)
 
-first_response = response.history[0]
-second_response = response
+# Изучаем cookie's
+payload = {"login": "secret_login", "password": "secret_pass2"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data = payload)
+print(response1.text)
+print(response1.status_code)
+print(dict(response1.cookies))
 
-print(first_response.url)
-print(second_response.url)
+cookie_value = response1.cookies.get("auth_cookie")
 
+cookies = {}
+if cookie_value is not None:
+    cookies.update({"auth_cookie": cookie_value})
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies = cookies)
+print(response2.text)
 
